4ec4941581f9f41483b4070694ccac6e2e3bbe77
Natee <gg977120@gmail.com>
2022-08-04 23:42:41 +0700

    done.

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-const profilePicture = document.getElementById('div-user-card');
+const profilePicture = document.getElementById('img-profile');
 const profileName = document.getElementById('p-name');
 const profileGenser = document.getElementById('span-gender-icon');
 const profileAddress = document.getElementById('p-address');
 const profileEmail = document.getElementById('p-email');
 const btnGenerateMore = document.getElementById('btn-random');
 
-btnGenerateMore.onclick = async() => {
+btnGenerateMore.onclick = async() => { getRandomProfile(); }
+  
+getRandomProfile = async() => {
+  document.getElementById('div-user-card').style.display = "none";
+  document.getElementById('div-loading-card').style.display = "";
+  profileGenser.innerText = '';
   const resp = await axios.get("https://randomuser.me/api/");
-  const user = resp.data.result;
+  const user = resp.data.results[0];
   profilePicture.src = user.picture.large;
-  profileName.innerText = (user.name.first + user.name.last);
-  profileAddress.innerText = user.location.city + user.location.state + user.location.country + user.location.postcode;
+  profileName.innerText = (user.name.first + ' ' + user.name.last);
+  profileAddress.innerText = user.location.city + ' ' + user.location.state + ' ' + user.location.country + ' ' + user.location.postcode;
   profileEmail.innerText = user.email;
   if(user.gender == 'male') profileGenser.innerText = '👨';
   if(user.gender == 'female') profileGenser.innerText = '👩';
-
+  document.getElementById('div-user-card').style.display = "";
+  document.getElementById('div-loading-card').style.display = "none";
 };
+
+getRandomProfile();
