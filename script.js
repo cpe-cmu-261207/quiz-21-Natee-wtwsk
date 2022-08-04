895bcbbb69ddb2d82370ccc9177618f365ff09b2
Natee <gg977120@gmail.com>
2022-08-04 14:09:36 +0700

    ...

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-async function callApi() {
+const profilePicture = document.getElementById('div-user-card');
+const profileName = document.getElementById('p-name');
+const profileGenser = document.getElementById('span-gender-icon');
+const profileAddress = document.getElementById('p-address');
+const profileEmail = document.getElementById('p-email');
+const btnGenerateMore = document.getElementById('btn-random');
+
+btnGenerateMore.onclick = async() => {
   const resp = await axios.get("https://randomuser.me/api/");
-}
+  const user = resp.data.result;
+  profilePicture.src = user.picture.large;
+  profileName.innerText = (user.name.first + user.name.last);
+  profileAddress.innerText = user.location.city + user.location.state + user.location.country + user.location.postcode;
+  profileEmail.innerText = user.email;
+  if(user.gender == 'male') profileGenser.innerText = '👨';
+  if(user.gender == 'female') profileGenser.innerText = '👩';
+
+};
